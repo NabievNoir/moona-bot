54674823d5126f8f1ddc0043cc090423b9dd0879
NabievNoir <107167575+NabievNoir@users.noreply.github.com>
2026-05-24 19:37:54 +0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -9,7 +9,7 @@
 // ============================================================
 
 const BOT_TOKEN    = process.env.8987560398:AAGQ-3lfsvDiloHwbiC_c4bqwIhDNpnH0HQ;
-const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_URL = process.env.https://pedchyiwcnlwzzgkmxxj.supabase.co;
 const ANON_KEY     = process.env.SUPABASE_ANON_KEY;
 
 const tg = async (method, body) => {
